4bfe6bbdb29b19109a7bfd33ea703f0c94e57318
Dacha <troewiha@gmail.com>
2015-11-15 21:15:33 +0200

    Invite HomeActivity

---
+++ app/src/main/java/com/myapplication2/app/HomeActivity.java
@@ -0,0 +1,12 @@
+package com.myapplication2.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
