88f3eb895c5d8778cb88140db67b8a8d9d9aa5c9
Dacha <troewiha@gmail.com>
2015-11-18 00:30:50 +0200

    debag

--- app/src/main/java/com/myapplication2/app/HomeActivity.java
+++ app/src/main/java/com/myapplication2/app/HomeActivity.java
@@ -35,17 +35,19 @@ public class HomeActivity extends Activity {
 
         progressBarPlayer1 = (ProgressBar) findViewById(R.id.progressBarPlayer1);
         progressBarPlayer2 = (ProgressBar) findViewById(R.id.progressBarPlayer2);
-        progressBarPlayer1.setMax(100);
+
     }
 
 
     class play extends AsyncTask<Void, Integer, Void>{
 
+        private int progress = 0;
+
         @Override
         protected Void doInBackground(Void... params) {
-            while (progressBarPlayer1.getProgress() < 100)
-                progressBarPlayer1.setProgress(progressBarPlayer1.getProgress() + (int)Math.random()*5);
-                publishProgress(progressBarPlayer1.getProgress() );
+            while (progress < 100)
+                progress++;
+                publishProgress(progress);
             SystemClock.sleep(1000);
             return null;
         }
@@ -54,18 +56,21 @@ public class HomeActivity extends Activity {
         protected void onPreExecute() {
             super.onPreExecute();
             Toast.makeText(HomeActivity.this, "Run", Toast.LENGTH_LONG);
+            System.out.println("0000000000000000000000000000000000000000000000000000000000000000000000000000000");
+
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
             Toast.makeText(HomeActivity.this, "END", Toast.LENGTH_SHORT);
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         }
 
         @Override
         protected void onProgressUpdate(Integer... values) {
             super.onProgressUpdate(values);
-            progressBarPlayer1.getProgress();
+            progressBarPlayer1.setProgress(values[0]);
         }
     }
 
